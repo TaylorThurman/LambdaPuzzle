06dffcf3198657349e7a50bbae431ba7b09f7358
taylor <taylor.thurman@astontech.com>
2019-09-25 13:01:55 -0500

    Adjustments made to scramble the puzzle and a little more clarity when reading output to console

--- src/java/gitbox/domain/individuals/LambdaDirector.java
+++ src/java/gitbox/domain/individuals/LambdaDirector.java
@@ -28,98 +28,38 @@ public class LambdaDirector {
 
     // remove Mr. MeeSeeks phrase from the string
     public static Function<String, String> removeMeeSeek = string -> {
-
-        String firstHalf = string.substring(0, string.indexOf('_'));
-        String secondHalf = string.substring(string.lastIndexOf('_') + 1, string.length());
-        String newString = firstHalf + secondHalf;
-
-        return newString;
+        return "";
     };
 
     // decrypt with StringCipher
     public static BiFunction<String, StringCipher, String> decrypt = (string, cipher) -> {
-        return cipher.decrypt(string, cipherTypes.get(3));
+        return "";
     };
 
     // A basic caesar cipher. Figure out which index it was switched to.
     // Use alphabet array list above to decrypt
     public static Function<String, String> caesarCipher = string -> {
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < string.length(); i++) {
-            int indexOfOldLetter = alphabet.indexOf(Character.toString(string.charAt(i)));
-            int indexOfNewLetter;
-            if (indexOfOldLetter > 2) {
-                indexOfNewLetter = indexOfOldLetter - 3;
-            } else {
-                int tempIndex = indexOfOldLetter - 3;
-                indexOfNewLetter = alphabet.size() + tempIndex;
-            }
-            sb.append(alphabet.get(indexOfNewLetter));
-        }
-
-        return sb.toString();
+        return "";
     };
 
     // for each word replace the first set of numbers with the character
     public static Function<String, String> revertNumberBeginning = string -> {
-
-        String[] splitString = string.split(" ");
-        StringBuilder finalString = new StringBuilder();
-
-        for (int i = 0; i < splitString.length; i++) {
-            String s = splitString[i];
-            String temp = s.replace(s.charAt(0), s.charAt(s.length()-1)) ;
-            temp = temp.substring(0, temp.length()-1);
-            finalString.append(temp);
-            if (i < splitString.length-1) {
-                finalString.append(" ");
-            }
-        }
-
-        return finalString.toString();
+        return "";
     };
 
     // turn every number into a character
     public static Function<String, String> listOfStringNumbers = string -> {
-
-        String periodsToSpaces = string.replace(".", " ");
-        String[] splitString = periodsToSpaces.split(" ");
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < splitString.length; i++) {
-            String temp = splitString[i];
-            int tempInt = Integer.parseInt(temp);
-            sb.append(Character.toString((char) tempInt));
-        }
-
-        return sb.toString();
+        return "";
     };
 
     // reverse the numbers in the string
     public static Function<String, String> individualNumbers = string -> {
-
-        String periodsToSpaces = string.replace(".", " ");
-        String[] splitString = periodsToSpaces.split(" ");
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < splitString.length; i++) {
-            StringBuilder temp = new StringBuilder(splitString[i]);
-            sb.append(temp.reverse());
-            if (i < splitString.length - 1) {
-                sb.append(".");
-            }
-        }
-
-        return sb.toString();
+        return "";
     };
 
     // reverse the string
     public static Function<String, String> reverseString = string -> {
-
-        StringBuilder sb = new StringBuilder(string);
-
-        return sb.reverse().toString();
+        return "";
     };
 
     /** NEED A HINT? CHECKOUT gitbox.include.show.Hints **/
---
+++ src/java/gitbox/include/Hints.java
@@ -0,0 +1,12 @@
+package gitbox.include;
+
+public class Hints {
+
+    public final String removeMeeSeek = "There is a specific string you must remove that starts and ends with _";
+    public final String decrypt = "Trying looking at the provided StringCipher for clues";
+    public final String caesarCipher = "It is only moved by an index less than 10";
+    public final String revertNumberBeginning = "The first letter is swapped with the index. Find where it went";
+    public final String listOfStringNumbers = "The numbers that the letters were switched to are based on a primitive";
+    public final String individualNumbers = "Misleading method. Similar to reverse string";
+    public final String reverseString = "ti esrever tsuJ";
+}
